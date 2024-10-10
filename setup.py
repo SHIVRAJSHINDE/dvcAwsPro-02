07fc4836902d3476f651b7ffe6bffe816722a1f0
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2024-10-10 10:25:30 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='EMOTION DETECTION USING AWS',
+    author='SHIVRAJ SHINDE',
+    license='MIT',
+)
